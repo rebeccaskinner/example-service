b859b2226475ff1fe091b0fe671766a4c5ef712b
Rebecca Skinner <rebecca@rebeccaskinner.net>
2017-02-10 11:21:19 -0600

    re-assign copyright to asteris, llc

--- service.c
+++ service.c
@@ -1,4 +1,4 @@
-// Copyright (c) 2017 Rebecca Skinner
+// Copyright (c) 2017 Asteris, LLC
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
